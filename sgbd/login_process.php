a12258795524ca3118f534df585fa6943e69aa58
Mihneass <48487418+Mihneass@users.noreply.github.com>
2019-05-28 22:08:02 +0300

    Add files via upload

--- sgbd/login_process.php
+++ sgbd/login_process.php
@@ -6,6 +6,7 @@ if (!$conn) {
    exit;
 }
 $enquiry="BEGIN GET_ONE_USER"."("."'".$_POST['uname']."'".",:pass,:id_user);end;";
+//$enquiry="BEGIN GET_ONE_USER"."("."'"."'".'||a;drop table membership'."'".",:pass,:id_user);end;";
 $stid = oci_parse($conn, $enquiry);
 oci_bind_by_name($stid,':pass',$rpassword,40);
 oci_bind_by_name($stid,':id_user',$id_user,40);
@@ -16,7 +17,8 @@ $rc=$row['MRFC'];
 oci_execute($rc);
 $rc_row = oci_fetch_array($rc, OCI_ASSOC+OCI_RETURN_NULLS);
 */
-if($rpassword==null){setcookie("is_logged","USER_NOT_FOUND",time()+(86400*10),"/");
+echo $rpassword;
+if($rpassword==='USER NOT FOUND'){setcookie("is_logged","USER_NOT_FOUND",time()+(86400*10),"/");
             echo $enquiry;
             header('Location: /sgbd/index.php');
             }
